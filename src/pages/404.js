2406e1d5e6e0696fc648af9be3545846fa32a33d
hafizakbar19 <hafizakbar19@gmail.com>
2023-08-01 13:07:40 +0500

    custom 404

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import Navbar from "@/components/navbar";
+
+
+export default function Error() {
+  return (
+    <div>
+        <Navbar />
+        <h1>Sorry page not found</h1>
+        <p>Our team looking into this matter and working hard to resolve the issue as soon as possible</p>
+    </div>
+  )
+}
