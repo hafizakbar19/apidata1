f4131ae7fc667cb361c2d555845f9ea80e6ef63a
hafizakbar19 <hafizakbar19@gmail.com>
2023-08-01 19:35:07 +0500

    .

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,30 @@
 import Navbar from "@/components/navbar";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 
 export default function Error() {
+    const router = useRouter();
+    const handleInput = () => {
+        router.push("/");
+    }
+
+    useEffect(
+        ()=>{
+            setTimeout(() => {
+               router.push("/") 
+            }, 3000);
+        },[]
+    );
   return (
     <div>
         <Navbar />
         <h1>Sorry page not found</h1>
-        <p>Our team looking into this matter and working hard to resolve the issue as soon as possible</p>
+        <p>Our team is looking into this matter and working hard to resolve the issue as soon as possible</p>
+        <Link href="/">
+        Back to Home
+        </Link>
     </div>
   )
 }
